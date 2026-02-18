41afdf0aae452f4498692dd9742d8d3385a02359
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-02-18 19:50:16 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,14 +1,21 @@
 package com.devsuperior.dscatalog.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
-     private Long id;
-     private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-     public Category() {}
+    private String name;
+
+    public Category() {}
 
     public Category(Long id, String name) {
          this.id = id;
